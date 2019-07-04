4799ecb395685020c3288533d15d49901d60f3e8
Joy Lin <vvlnote@hotmail.com>
2019-07-04 01:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const user = '<YOUR_USERNAME>';
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  fetch('https://github.com/login/oauth/authorize')
+  .then(res => res.json())
+  .then(json => console.log(json));
   return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const token = "567c3f0355831495f4f66ddfae4eaee5eed8880a";
   //use fetch to fork it!
+  console.log(`https://api.github.com/repos/${repo}/forks`);
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
